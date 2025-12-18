f7eb44867f4920c8f5b7a10519c9db60e0641d8b
Julia Bisewska <bisewskaj@gmail.com>
2025-12-18 19:21:07 +0100

    feat: add api service

--- mobile/app/src/main/java/com/woofdetect/network/ApiService.kt
+++ mobile/app/src/main/java/com/woofdetect/network/ApiService.kt
@@ -1,5 +1,31 @@
 package com.woofdetect.network
 
-class ApiService {
-//    ApiService → endpointy (@POST, @Multipart)
+import com.woofdetect.network.dto.DogAnalysisResponse
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.*
+
+/*
+    API endpoints
+    Retrofit implements
+ */
+
+interface ApiService {
+
+    // POST /getDogBreedInfo
+    @Multipart
+    @POST("getDogBreedInfo")
+    suspend fun analyzeDogPhoto(
+        @Part photo: MultipartBody.Part,
+        @Header("Authorization") authorization: String? = null
+    ): Response<DogAnalysisResponse>
+
+    // POST /submitDogBreedFeedback
+    @POST
+    @Headers("Content-Type: application/json")
+    suspend fun submitFeedback(
+        @Body body: Map<String, Map<String, String>>,
+        @Header("Authorization") authorization: String? = null
+    ): Response<String>
+
 }
