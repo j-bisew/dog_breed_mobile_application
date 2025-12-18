a5f3fff5cf53020a52c0cae557353ac33d112fc8
Julia Bisewska <bisewskaj@gmail.com>
2025-12-18 23:07:26 +0100

    refactor main activity to add token manager

--- mobile/app/src/main/java/com/woofdetect/MainActivity.kt
+++ mobile/app/src/main/java/com/woofdetect/MainActivity.kt
@@ -2,18 +2,39 @@ package com.woofdetect
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.woofdetect.auth.TokenManager
+import com.woofdetect.network.ApiClient
 import com.woofdetect.ui.auth.AuthChoiceFragment
 import com.woofdetect.ui.main.HomeFragment
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var tokenManager: TokenManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ApiClient.initialize(this)
+
+        tokenManager = TokenManager(this)
+
         if (savedInstanceState == null) {
+            lifecycleScope.launch {
+                val token = tokenManager.getToken().first()
+
+                val startFragment = if (token.isNullOrEmpty()) {
+                    AuthChoiceFragment()
+                } else {
+                    HomeFragment()
+                }
+
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, AuthChoiceFragment())
+                .replace(R.id.fragmentContainer, startFragment)
                 .commit()
+            }
         }
     }
 }
